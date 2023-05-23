24df89f98d0982be6cc54868c47f0f576e96d81f
Vishnou Rasenthiram <sathuvishnou@gmail.com>
2023-05-23 20:09:47 +0200

    repare

--- main.py
+++ main.py
@@ -102,7 +102,7 @@ async def on_ready():
             await guuruuchan.send("N'oubliez pas de faire votre Loldle du jour !")
             await guuruuchan.send("https://loldle.net/")
         if currect_daay in ["Monday","Tuesday","Wednesday","Thursday"] and (current_time>="20:00" and current_time<"20:10"):
-            msg = await guuruuchanflame.send("Alors,@Guuruu#2278 t'es allé à la salle ?")
+            msg = await guuruuchanflame.send("Alors,<@185191654255362048> t'es allé à la salle ?")
             global FLAMEID
             FLAMEID=msg.id
             await msg.add_reaction("✅")

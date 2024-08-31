aab49ae1bb5f15355291add0ef1f5a246f5b61fc
VishnuRasenthiram <vishnuRasenthiram@gmail.com>
2024-09-01 00:45:12 +0200

    ajout vocal temporaire pour serveur Tsipo

--- main.py
+++ main.py
@@ -594,10 +594,11 @@ async def ban(ctx, member : discord.Member, *, reason = None):
 async def on_voice_state_update(member,before,after):
     if not after.channel==None:
 
-        if after.channel.id ==CHAN_VOC:
-
-
-            chan = await after.channel.clone(name=f'◜⏳◞{member.display_name}')
+        if after.channel.id ==CHAN_VOC or after.channel.id==1235199112257994792 :
+            if after.channel.id ==CHAN_VOC:
+                chan = await after.channel.clone(name=f'◜⏳◞{member.display_name}')
+            else :
+                chan = await after.channel.clone(name=f'◜⏳◞Gaming Session')
             await member.move_to(chan)
 
 

08c5c9bd24a8e0287fce88ed9cd076ee3b717176
VishnuRasenthiram <vishnuRasenthiram@gmail.com>
2025-01-15 23:46:22 +0100

    bugfix historique

--- leagueOfFunction.py
+++ leagueOfFunction.py
@@ -168,9 +168,7 @@ class LOF:
         await interaction.response.defer()
         
         try:
-                print("Début de la requête")
                 image =creeImageHistorique(puuid,region)
-                print("Image créée")
                 img_bytes=BytesIO()
                 image.save(img_bytes,format='PNG')
                 img_bytes.seek(0)
